0d1b3f234a3910acc341b05929460a23993083e8
Gavesh01 <117095945+Gavesh01@users.noreply.github.com>
2023-04-20 21:47:15 +0530

    change searchMealRecipeByIngredient to get the error message

--- app/src/main/java/com/example/mealmaster/SearchByIngredientActivity.kt
+++ app/src/main/java/com/example/mealmaster/SearchByIngredientActivity.kt
@@ -161,90 +161,91 @@ class SearchByIngredientActivity : AppCompatActivity(){
     private fun searchMealRecipeByIngredient(ingredients: String): List<Meal>{
 
 
-        // here can't access fully details in one step.
-        // first get meals summery, by filtering their ingredients.
-        // ------
-
         val urlSummary = URL("https://www.themealdb.com/api/json/v1/$apiKeyNumber/filter.php?i=$ingredients")
         // read what api returned
+
         val jsonStringOutSummery = urlSummary.readText()
         // convert output to the Json objects
-        val summeryInJsonForm = JSONObject(jsonStringOutSummery)
-        // get Json array called meals
-        val mealsSummery = summeryInJsonForm.getJSONArray("meals")
         // create a mutable list object to hold Meal instances.
         val detailsOfRecipe = mutableListOf<Meal>()
 
-        for (i in 0 until mealsSummery.length()){
-            val detailsOfTheMeal =  mealsSummery.getJSONObject(i)
-            // get meal id
-            val idMeal = detailsOfTheMeal.getInt("idMeal")
-
-            val url = URL("https://www.themealdb.com/api/json/v1/$apiKeyNumber/lookup.php?i=$idMeal")
-
-            val jsonStrings = url.readText()
-            // convert output to the Json objects
-            val jsonOb = JSONObject(jsonStrings)
-
+        val x="{\"meals\":null}"
+        if(jsonStringOutSummery!=x){
+            val summeryInJsonForm = JSONObject(jsonStringOutSummery)
             // get Json array called meals
-            val meals = jsonOb.getJSONArray("meals")
+            val mealsSummery = summeryInJsonForm.getJSONArray("meals")
 
-            for (i in 0 until meals.length()){
-                // getting meal by meal
-                val meal = meals.getJSONObject(i)
 
-                val strMeal = meal.getString("strMeal")
-                val strDrinkAlternate = meal.getString("strDrinkAlternate")
-                val strCategory = meal.getString("strCategory")
-                val strArea = meal.getString("strArea")
-                val strInstructions = meal.getString("strInstructions")
-                val strMealThumb = meal.getString("strMealThumb")
-                val strTags = meal.getString("strTags")
-                val strYoutube = meal.getString("strYoutube")
-                val strIngredient1 = meal.getString("strIngredient1")
-                val strIngredient2 = meal.getString("strIngredient2")
-                val strIngredient3 = meal.getString("strIngredient3")
-                val strIngredient4 = meal.getString("strIngredient4")
-                val strIngredient5 = meal.getString("strIngredient5")
-                val strIngredient6 = meal.getString("strIngredient6")
-                val strIngredient7 = meal.getString("strIngredient7")
-                val strIngredient8 = meal.getString("strIngredient8")
-                val strIngredient9 = meal.getString("strIngredient9")
-                val strIngredient10 = meal.getString("strIngredient10")
-                val strIngredient11 = meal.getString("strIngredient11")
-                val strIngredient12 = meal.getString("strIngredient12")
-                val strIngredient13 = meal.getString("strIngredient13")
-                val strIngredient14 = meal.getString("strIngredient14")
-                val strIngredient15 = meal.getString("strIngredient15")
-                val strIngredient16 = meal.getString("strIngredient16")
-                val strIngredient17 = meal.getString("strIngredient17")
-                val strIngredient18 = meal.getString("strIngredient18")
-                val strIngredient19 = meal.getString("strIngredient19")
-                val strIngredient20 = meal.getString("strIngredient20")
-                val strMeasure1 = meal.getString("strMeasure1")
-                val strMeasure2 = meal.getString("strMeasure2")
-                val strMeasure3 = meal.getString("strMeasure3")
-                val strMeasure4 = meal.getString("strMeasure4")
-                val strMeasure5 = meal.getString("strMeasure5")
-                val strMeasure6 = meal.getString("strMeasure6")
-                val strMeasure7 = meal.getString("strMeasure7")
-                val strMeasure8 = meal.getString("strMeasure8")
-                val strMeasure9 = meal.getString("strMeasure9")
-                val strMeasure10 = meal.getString("strMeasure10")
-                val strMeasure11 = meal.getString("strMeasure11")
-                val strMeasure12 = meal.getString("strMeasure12")
-                val strMeasure13 = meal.getString("strMeasure13")
-                val strMeasure14 = meal.getString("strMeasure14")
-                val strMeasure15 = meal.getString("strMeasure15")
-                val strMeasure16 = meal.getString("strMeasure16")
-                val strMeasure17 = meal.getString("strMeasure17")
-                val strMeasure18 = meal.getString("strMeasure18")
-                val strMeasure19 = meal.getString("strMeasure19")
-                val strMeasure20 = meal.getString("strMeasure20")
-                val strSource = meal.getString("strSource")
-                val strImageSource = meal.getString("strImageSource")
-                val strCreativeCommonsConfirmed = meal.getString("strCreativeCommonsConfirmed")
-                val dateModified = meal.getString("dateModified")
+            for (i in 0 until mealsSummery.length()){
+                val detailsOfTheMeal =  mealsSummery.getJSONObject(i)
+                // get meal id
+                val idMeal = detailsOfTheMeal.getInt("idMeal")
+
+                val url = URL("https://www.themealdb.com/api/json/v1/$apiKeyNumber/lookup.php?i=$idMeal")
+
+                val jsonStrings = url.readText()
+                // convert output to the Json objects
+                val jsonOb = JSONObject(jsonStrings)
+
+                // get Json array called meals
+                val meals = jsonOb.getJSONArray("meals")
+
+                for (i in 0 until meals.length()){
+                    // getting meal by meal
+                    val meal = meals.getJSONObject(i)
+
+                    val strMeal = meal.getString("strMeal")
+                    val strDrinkAlternate = meal.getString("strDrinkAlternate")
+                    val strCategory = meal.getString("strCategory")
+                    val strArea = meal.getString("strArea")
+                    val strInstructions = meal.getString("strInstructions")
+                    val strMealThumb = meal.getString("strMealThumb")
+                    val strTags = meal.getString("strTags")
+                    val strYoutube = meal.getString("strYoutube")
+                    val strIngredient1 = meal.getString("strIngredient1")
+                    val strIngredient2 = meal.getString("strIngredient2")
+                    val strIngredient3 = meal.getString("strIngredient3")
+                    val strIngredient4 = meal.getString("strIngredient4")
+                    val strIngredient5 = meal.getString("strIngredient5")
+                    val strIngredient6 = meal.getString("strIngredient6")
+                    val strIngredient7 = meal.getString("strIngredient7")
+                    val strIngredient8 = meal.getString("strIngredient8")
+                    val strIngredient9 = meal.getString("strIngredient9")
+                    val strIngredient10 = meal.getString("strIngredient10")
+                    val strIngredient11 = meal.getString("strIngredient11")
+                    val strIngredient12 = meal.getString("strIngredient12")
+                    val strIngredient13 = meal.getString("strIngredient13")
+                    val strIngredient14 = meal.getString("strIngredient14")
+                    val strIngredient15 = meal.getString("strIngredient15")
+                    val strIngredient16 = meal.getString("strIngredient16")
+                    val strIngredient17 = meal.getString("strIngredient17")
+                    val strIngredient18 = meal.getString("strIngredient18")
+                    val strIngredient19 = meal.getString("strIngredient19")
+                    val strIngredient20 = meal.getString("strIngredient20")
+                    val strMeasure1 = meal.getString("strMeasure1")
+                    val strMeasure2 = meal.getString("strMeasure2")
+                    val strMeasure3 = meal.getString("strMeasure3")
+                    val strMeasure4 = meal.getString("strMeasure4")
+                    val strMeasure5 = meal.getString("strMeasure5")
+                    val strMeasure6 = meal.getString("strMeasure6")
+                    val strMeasure7 = meal.getString("strMeasure7")
+                    val strMeasure8 = meal.getString("strMeasure8")
+                    val strMeasure9 = meal.getString("strMeasure9")
+                    val strMeasure10 = meal.getString("strMeasure10")
+                    val strMeasure11 = meal.getString("strMeasure11")
+                    val strMeasure12 = meal.getString("strMeasure12")
+                    val strMeasure13 = meal.getString("strMeasure13")
+                    val strMeasure14 = meal.getString("strMeasure14")
+                    val strMeasure15 = meal.getString("strMeasure15")
+                    val strMeasure16 = meal.getString("strMeasure16")
+                    val strMeasure17 = meal.getString("strMeasure17")
+                    val strMeasure18 = meal.getString("strMeasure18")
+                    val strMeasure19 = meal.getString("strMeasure19")
+                    val strMeasure20 = meal.getString("strMeasure20")
+                    val strSource = meal.getString("strSource")
+                    val strImageSource = meal.getString("strImageSource")
+                    val strCreativeCommonsConfirmed = meal.getString("strCreativeCommonsConfirmed")
+                    val dateModified = meal.getString("dateModified")
 
                 detailsOfRecipe.add(
                     Meal(
@@ -307,10 +308,11 @@ class SearchByIngredientActivity : AppCompatActivity(){
             }
 
         }
-        return detailsOfRecipe
+            return detailsOfRecipe
+        }else{
+            return(emptyList())
+        }
     }
-
-
 
 
 
